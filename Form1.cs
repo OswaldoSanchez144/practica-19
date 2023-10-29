62de190f5b2671dfe6145b638690c003fa5922e9
DELL-1 <DELL-1@DESKTOP-GAMS896>
2023-10-29 17:18:07 -0500

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practica_19
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double cal1,cal2,cal3,cal4,promedio;
+            cal1=double.Parse(textBox1.Text);
+            cal2=double.Parse(textBox2.Text);
+            cal3=double.Parse(textBox3.Text);
+            cal4=double.Parse(textBox4.Text);
+            promedio = (cal1 + cal2 + cal3 + cal4) / 4;
+            textBox5.Text= promedio.ToString();
+            if (promedio >= 6)MessageBox.Show("APROBADO"); else MessageBox.Show("REPROBADO");
+        }
+    }
+}
